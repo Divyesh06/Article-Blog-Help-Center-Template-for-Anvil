497e906d9e28232b92ba9ad441a89f489959598f
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2024-01-09 12:49:43 +0000

    Edited note_on_search_engine_indexing

--- client_code/note_on_search_engine_indexing.py
+++ client_code/note_on_search_engine_indexing.py
@@ -4,7 +4,19 @@ While Single Page Apps built with Anvil always face difficulty with indexing.
 
 Here is how this app aims to improve your SEO
 
-1. Internal Linking
+1. Sitemap
 
-When crawling this website, crawlers will be able to find links to all articles and crawl them.
+Sitemaps allow search crawlers to easily discover all URLs that are present in your website and crawl them separately. 
+
+A sitemap is automatically maintained for you. You can find it at <your app url>/_/api/sitemap.xml
+
+However, you will have to submit the sitemap to Google Search Console (or other search engines)
+https://www.semrush.com/blog/submit-sitemap-to-google/
+https://www.bing.com/webmasters/help/Sitemaps-3b5cf6ed
+
+
+2. Internal Linking
+
+When crawling this website, crawlers might also be able to find links to all articles and crawl them separately. (Not very reliable though)
+
 """
