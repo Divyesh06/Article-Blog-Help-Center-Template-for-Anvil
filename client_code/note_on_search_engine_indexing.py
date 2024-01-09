1e2372acad1faba91f3b123b1d9afaed1f053b24
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2024-01-09 12:39:34 +0000

    Created note_on_search_engine_indexing; Edited Artice_Link

---
+++ client_code/note_on_search_engine_indexing.py
@@ -0,0 +1,10 @@
+"""
+This template is designed with the specific purpose of making sure that all your pages are indexed on Google or other search engines easily.
+While Single Page Apps built with Anvil always face difficulty with indexing. 
+
+Here is how this app aims to improve your SEO
+
+1. Internal Linking
+
+When crawling this website, crawlers will be able to find links to all articles and crawl them.
+"""
